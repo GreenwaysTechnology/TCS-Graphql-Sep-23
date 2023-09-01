f701107937737ce8eab0a6062839d7b391c7e259
Subramanian Murugan <sasubramanian_md@hotmail.com>
2023-09-01 19:54:14 +0530

    apollo server

---
+++ apollo-gettingstarted/dist/index-helloschema.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
+//1.Define Schema 
+const typeDefs = `
+ #Define api
+ type Query{
+   hello:String    
+ } 
+
+`;
+//2.Biz logic for hello Query : Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return "Hello Graphql";
+        }
+    },
+    //Mutation
+    //Subscription
+};
+//3.We need to deploy the schema and bind with resolver 
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+});
+//4.Start web server (Express.js)
+const { url } = await startStandaloneServer(server, {
+    listen: {
+        port: 4000
+    }
+});
+console.log(`Apollo Server is Ready ${url}`);
